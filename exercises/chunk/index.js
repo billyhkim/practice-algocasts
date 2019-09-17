b92a98f6fc10cd72e79a07471b63444c3b4da37e
Billy Kim <billyhkim.dev@gmail.com>
2019-09-17 14:12:34 -0700

    chunk first implementation

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let containerArray = [];
+  let chunkArray = [];
+  for (let i = 0; i <= array.length; i++) {
+    if (chunkArray.length !== size) {
+      chunkArray.push(array[i]);
+    } else if (chunkArray.length === size) {
+      containerArray.push(chunkArray);
+      chunkArray = [];
+      chunkArray.push(array[i]);
+    } else if (i === array.length) {
+      chunkArray.push(array[i]);
+      containerArray.push(chunkArray);
+    }
+  }
+  return containerArray;
+}
 
 module.exports = chunk;
