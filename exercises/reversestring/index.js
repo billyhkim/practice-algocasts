0a74308370cb21f4daea380f4976e4e575340824
Billy Kim <billyhkim.dev@gmail.com>
2019-09-17 02:00:44 -0700

    reversestring without reverse method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,7 +8,15 @@
 
 function reverse(str) {
   // method 1 using reverse method
-  return str.split('').reverse().join('');
+  // return str.split('').reverse().join('');
+
+  // method 2 without reverse method
+  // use a for loop to iterate backwards thru the string and concat it to some variable
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
 module.exports = reverse;
