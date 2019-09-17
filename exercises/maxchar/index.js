4fbeaa53acff9d83f9cfbe4cf77f4e830f5a0ba1
Billy Kim <billyhkim.dev@gmail.com>
2019-09-17 12:42:17 -0700

    maxchar completed using for in

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,15 +6,36 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-  let charCountObj = {};
-  for (let i = 0; i < str.length; i++) {
-    if (!charCountObj.hasOwnProperty(str[i])) {
-      charCountObj[str[i]] = 1;
-    } else {
-      charCountObj[str[i]]++;
+  // 1. create map of character freq obj
+  let characterMapObj = {};
+  // 1a. using for loop with nested conditional
+  // for (let i = 0; i < str.length; i++) {
+  //   if (!characterMapObj.hasOwnProperty(str[i])) {
+  //     characterMapObj[str[i]] = 1;
+  //   } else {
+  //     characterMapObj[str[i]]++;
+  //   }
+  // }
+
+  // 1b. map character obj using for of
+  for (let character of str) {
+    characterMapObj[character] ? characterMapObj[character]++ : characterMapObj[character] = 1;
+  }
+
+  // 2. iterate over characterMapObj and determine max value key
+  // 2a. using reduce after turning keys into array
+  // return Object.keys(characterMapObj).reduce((a, b) => characterMapObj[a] > characterMapObj[b] ? a : b);
+
+  // 2b. using for in
+  let max = 0;
+  let maxCharacterKey = '';
+  for (let character in characterMapObj) {
+    if (characterMapObj[character] > max) {
+      max = characterMapObj[character];
+      maxCharacterKey = character;
     }
   }
-  return Object.keys(charCountObj).reduce((a, b) => charCountObj[a] > charCountObj[b] ? a : b);
+  return maxCharacterKey;
 }
 
 module.exports = maxChar;
