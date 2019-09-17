7a519b835d8d7a1279c295576c2690685cc331bb
Billy Kim <billyhkim.dev@gmail.com>
2019-09-17 02:18:34 -0700

    palindrome completed exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversedStr = str.split('').reverse().join('');
+  
+  if (str === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = palindrome;
