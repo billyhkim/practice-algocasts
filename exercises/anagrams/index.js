87adf76dd25bfa8e1d0ec8d7c2985ef8227feb5a
Billy Kim <billyhkim.dev@gmail.com>
2019-09-22 23:27:29 -0700

    anagrams completed with regex and sort

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,36 +9,44 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  let charMap1 = {};
-  let charMap2 = {};
+  // first attempt
+  // let charMap1 = {};
+  // let charMap2 = {};
 
-  let string1 = stringA.toLowerCase();
-  for (let char of string1) {
-    if (char === ' ' || char === '!' || char === ',' || char === '.') {
-      continue;
-    } else if (!charMap1[char]) {
-      charMap1[char] = 1;
-    } else if (charMap1[char]) {
-      charMap1[char]++;
-    }
-  }
+  // let string1 = stringA.toLowerCase();
+  // for (let char of string1) {
+  //   if (char === ' ' || char === '!' || char === ',' || char === '.') {
+  //     continue;
+  //   } else if (!charMap1[char]) {
+  //     charMap1[char] = 1;
+  //   } else if (charMap1[char]) {
+  //     charMap1[char]++;
+  //   }
+  // }
 
-  let string2 = stringB.toLowerCase();
-  for (let char of string2) {
-    if (char === ' ' || char === '!' || char === ',' || char === '.') {
-      continue;
-    } else if (!charMap2[char]) {
-      charMap2[char] = 1;
-    } else if (charMap2[char]) {
-      charMap2[char]++;
-    }
-  }
+  // let string2 = stringB.toLowerCase();
+  // for (let char of string2) {
+  //   if (char === ' ' || char === '!' || char === ',' || char === '.') {
+  //     continue;
+  //   } else if (!charMap2[char]) {
+  //     charMap2[char] = 1;
+  //   } else if (charMap2[char]) {
+  //     charMap2[char]++;
+  //   }
+  // }
 
-  if (charMap1 === charMap2) {
-    return true;
-  } else {
-    return false;
-  }
+  return stringA
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('') ===
+  stringB
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
 }
 
 module.exports = anagrams;
